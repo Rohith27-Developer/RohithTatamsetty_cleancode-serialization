032ed9dffd8281cfc1eabe0be0a947942fe9d672
Rohith <177r1a05b3@gmail.com>
2020-02-17 19:07:25 +0530

    Task4

---
+++ House_Construction/src/main/java/com/epam/pep/Construction.java
@@ -0,0 +1,8 @@
+package com.epam.pep;
+public class Construction {
+	public int construct(int total_area,int material_cost)
+	{
+		int total_cost=total_area*material_cost;
+		return total_cost;
+	}
+}
